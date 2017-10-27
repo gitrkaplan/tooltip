ba70a945f4ad73315a84cc51a802c1c43455299d
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-27 10:13:44 -0700

    Adds function to fetch data from backend and passes the results array to a function to generate a random number. Tooltip text is then populated by the result of the promise at the random index number. #1

--- public/main.js
+++ public/main.js
@@ -1,12 +1,25 @@
 const buttons = document.querySelectorAll('.button')
 
+const tooltips = () => {
+  return fetch('/tooltip/tips')
+    .then(res => res.json())
+    .then(array => randTooltip(array))
+}
+
+const randTooltip = array => {
+  let num = Math.floor(Math.random() * 24 + 0)
+  return array[num].tip
+}
+
 buttons.forEach(el => {
   el.addEventListener('mouseover', event => {
     const { top, bottom, left, right } = el.getBoundingClientRect()
-    if (top > 30 && window.innerWidth - right > 120) {
+    let tipText = el.getElementsByClassName('tooltiptext')
+    tooltips().then(result => (tipText[0].innerHTML = result))
+    if (bottom < 40 && window.innerWidth - right > 120) {
       el.classList.remove('top')
       el.classList.add('right')
-    } else if (bottom > 30 && window.innerWidth - right > 120) {
+    } else if (bottom > 40 && window.innerWidth - right > 120) {
       el.classList.remove('top')
       el.classList.add('bottom')
     } else if (left > 120 && window.innerHeight - top > 120) {
--- seed.js
+++ seed.js
@@ -10,20 +10,36 @@ MongoClient.connect(url, (err, db) => {
     .deleteMany({})
     .then(() => {
       tips
-        .insertMany([
-          {
-            tip: 'This is a left tooltip'
-          },
-          {
-            tip: 'This is a right tooltip'
-          },
-          {
-            tip: 'This is a bottom tooltip'
-          },
-          {
-            tip: 'This is a top tooltip'
-          }
-        ])
+        .insertMany(
+          [
+            'Most soccer players run 7 miles in a game.',
+            'The only 2 animals that can see behind itself without turning its head are the rabbit and the parrot.',
+            'Whip makes a cracking sound because its tip moves faster than the speed of sound.',
+            'It cost 7 million dollars to build the Titanic and 200 million to make a film about it.',
+            'When hippos are upset, their sweat turns red.',
+            'Every time you sneeze some of your brain cells die.',
+            'Your left lung is smaller than your right lung to make room for your heart.',
+            'Laughing lowers levels of stress hormones and strengthens the immune system. Six-year-olds laugh an average of 300 times a day. Adults only laugh 15 to 100 times a day.',
+            'Chewing gum while peeling onions will keep you from crying.',
+            'The Boeing 747 is capable of flying upside-down if it weren’t for the fact that the wings would shear off when trying to roll it over.',
+            'Never hold your nose and cover your mouth when sneezing, as it can blow out your eyeballs.',
+            'The world’s smartest pig, owned by a mathematics teacher in Madison, WI, memorized the multiplication tables up to 12.',
+            'In ancient Greece, children of wealthy families were dipped in olive oil at birth to keep them hairless throughout their lives.',
+            'Every Labrador retriever dreams about bananas once in a while.',
+            'Approximately one-sixth of your life is spent on Wednesdays.',
+            'You can actually sharpen the blades on a pencil sharpener by wrapping your pencils in aluminum foil before inserting them.',
+            '111,111,111 x 111,111,111 = 12,345,678,987,654,321',
+            '12 newborns will be given to the wrong parents daily.',
+            '123,000,000 cars are being driven down the U.S’s highways.',
+            '160 cars can drive side by side on the Monumental Axis in Brazil, the world’s widest road.',
+            'A cockroach can live several weeks with its head cut off.',
+            'A company in Taiwan makes dinnerware out of wheat, so you can eat your plate.',
+            'A cow produces 200 times more gas a day than a person.',
+            'A dime has 118 ridges around the edge.'
+          ].map(tip => ({
+            tip: tip
+          }))
+        )
         .catch(err => {
           console.error(err)
         })
